191e43182ad1df564b21385b33f657a1c601f868
Brennen-Beck <brennen_beck@yahoo.com>
2024-12-31 21:30:59 -0600

    Redirecting the hard coded API site to Azure.

--- scripts/modules/Globals.js
+++ scripts/modules/Globals.js
@@ -1,3 +1,3 @@
 export const LocalGameID=3;
 //export const TravellerAPIWebSiteURL ="https://localhost:7181/api";
-export const TravellerAPIWebSiteURL ="http://apitest.virtuallyprogramming.com/api";
+export const TravellerAPIWebSiteURL ="https://travellerapi-bzdwewbff8cufbcg.centralus-01.azurewebsites.net/api";
